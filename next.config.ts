b9995faf062724fb8dccab8f3a64dd496c54caa3
RIKI <rikinozomu2022@gmail.com>
2024-12-25 00:56:21 +0700

    fixed canvas error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: 'raw-loader',
+    });
+ 
+    return config;
+  },
   experimental: {
     extensionAlias: {
       '.js': ['.tsx', '.ts', '.jsx', '.js'],
